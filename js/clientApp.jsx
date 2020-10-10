b49b3b67b6a81f59779f663ada3be218dfbf4634
luke-h1 <luke.howsam@yahoo.com>
2020-10-10 16:09:05 +0100

    updates

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,28 +1,37 @@
+import React from "react";
+import { render } from "react-dom";
+
+const ce = React.createElement;
+
 const MyTitle = function (props) {
-  return React.createElement('div', null, React.createElement('h1', { style: { color: props.color } }, props.title));
+  return ce(
+    "div",
+    null,
+    ce("h1", { style: { color: props.color } }, props.title)
+  );
 };
 
 const MyFirstComponent = function () {
-  return React.createElement(
-    'div',
+  return ce(
+    "div",
     null,
-    React.createElement(MyTitle, {
-      title: 'Game of thrones',
-      color: 'YellowGreen',
+    ce(MyTitle, {
+      title: "Game of thrones",
+      color: "YellowGreen",
     }),
-    React.createElement(MyTitle, {
-      title: 'Stranger things',
-      color: 'GreenYellow',
+    ce(MyTitle, {
+      title: "Stranger things",
+      color: "GreenYellow",
     }),
-    React.createElement(MyTitle, {
-      title: 'Rick & Morty',
-      color: 'LimeGreen',
+    ce(MyTitle, {
+      title: "Rick & Morty",
+      color: "LimeGreen",
     }),
-    React.createElement(MyTitle, {
-      title: 'Silicon valley',
-      color: 'Lime',
+    ce(MyTitle, {
+      title: "Silicon valley",
+      color: "Lime",
     })
   );
 };
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'));
+render(ce(MyFirstComponent), document.getElementById("app"));
