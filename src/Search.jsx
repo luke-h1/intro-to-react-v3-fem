24e5a4a2447f5a81159e8f7f6fd52db5941b2825
luke-h1 <luke.howsam@yahoo.com>
2020-10-11 19:49:01 +0100

    add state to input

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,13 +1,37 @@
-
-import React from 'react';
+/* eslint-disable */
+import React, { Component } from 'react';
 import ShowCard from './showCard';
 import preload from '../data.json';
 
-const Search = () =>
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>;
-
+class Search extends Component {
+  state = {
+    searchTerm: '',
+  };
+  handleSearchTermChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    // HAPPENS ONCE IN CONSTRUCTOR & WORKS FOREVER AFTER...
+    // GUARANTEES IT WILL BE IN THE CORRECT CONTEXT
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>Svideo</h1>
+          <input
+            type="text"
+            placeholder="search..."
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTermChange}
+          />
+        </header>
+        <div>
+          {preload.shows.map((show) => (
+            <ShowCard key={show.imdbID} {...show} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 export default Search;
