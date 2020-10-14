7504e7f82575a0927a0522016ae9ee67620dda89
luke-h1 <luke.howsam@yahoo.com>
2020-10-14 08:42:26 +0100

    update

--- src/__tests__/Search.spec.jsx
+++ src/__tests__/Search.spec.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable */
 import React from 'react';
-import renderer from 'react-test-renderer';
 import Search from '../Search';
+import { shallow } from 'enzyme'; 
+
+
+console.log(`Node ENV: ${process.env.NODE_ENV}`);
 
 test('Search renders correctly', () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
 });
