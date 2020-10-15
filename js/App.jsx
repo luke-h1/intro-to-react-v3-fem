468a8c76f96ea26a92a6c611bc0167a7ebc375b6
luke-h1 <luke.howsam@yahoo.com>
2020-10-15 21:04:04 +0100

    update

--- js/App.jsx
+++ js/App.jsx
@@ -1,20 +1,28 @@
+// @flow
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Landing from './Landing';
 import Search from './Search';
-
+import preload from '../data.json';
+import Details from './Details';
 const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={Search} />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
+          <Route path='/details/:id' component={Details} />
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+    </Provider>
   </BrowserRouter>
 );
-
 export default App;
+// 7:51 URL search params
---
+++ js/Details.jsx
@@ -0,0 +1,9 @@
+// @flow
+
+import React from 'react';
+const Details = () => (  
+  <div className='details'>
+    <h1>hello</h1>
+  </div>
+) 
+export default Details;
